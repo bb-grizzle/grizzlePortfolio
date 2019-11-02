d54a4832ff4a7c3d435283fa4200445b031e969c
grizzle <firbigi1993@gmail.com>
2019-11-02 23:52:07 +0900

    -

--- source/js/loading.js
+++ source/js/loading.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 loader_text();
 
 //const box = document.querySelector(".box");
@@ -46,52 +45,3 @@ function loader_text(){
 	},300);
 }
 
-=======
-loader_text();
-
-//const box = document.querySelector(".box");
-//const boxFill = document.querySelector(".box-fill");
-//let boxLeft = -20;
-//
-//function changeBoxWidth() {
-//	box.style.width = "100%";
-//}
-//
-//function changeBoxPos() {
-//	boxLeft += 20;
-//	boxFill.style.left = boxLeft + "%";
-//	box.style.width = "50%";
-//}
-
-//function loader_rect() {
-//	setInterval(() => {
-//		changeBoxWidth();
-//		setTimeout(() => {
-//			changeBoxPos();
-//		}, 700);
-//
-//		if (boxLeft > 80) {
-//			boxLeft = -60;
-//			boxFill.style.display = "none";
-//		} else {
-//			boxFill.style.display = "block";
-//		}
-//
-//	}, 1400);
-//}
-
-function loader_text(){
-	console.log();
-	console.log('- loader_text -');
-	
-	const roll=["creator", "designer", "developer", "student", "lazybones", "father", "mother", "helper", "explorer", "visitor", "manager", "artist", "musician", "poor", "rich"];
-	
-	const p = document.querySelector('.load-p');
-	
-	setInterval(()=>{
-		const ranNumb = Math.floor(Math.random()*roll.length);
-		p.innerHTML = roll[ranNumb];
-	},300);
-}
-
->>>>>>> 2f1ad284b0876f78eab1098569b32e22947fd788
